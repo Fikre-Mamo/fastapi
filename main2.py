b9c24a7543b62e6ea949e52db93d2bc3a80e3375
Fikre Mamo <fikremichaelmamo@gmail.com>
2026-05-05 18:47:57 +0300

    created the pydantic model

--- main2.py
+++ main2.py
@@ -1,7 +1,9 @@
-from fastapi import FastAPI, Body, Request
+from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+from typing import Optional
 import uvicorn
+
 
 app = FastAPI()
 
@@ -20,11 +22,22 @@ class BOOK:
         self.rating = rating
     
 class BookRequest(BaseModel):
-    id: int
-    book_name: str
-    author_name: str
-    description: str
-    rating: float
+    id: Optional[int] = Field(description='ID is not neded on create', default=None)
+    book_name: str = Field(min_length=3, max_length=100)
+    author_name: str = Field(min_length=2, max_length=100)
+    description: str = Field(min_length=10, max_length=200)
+    rating: float = Field(ge=0, le=6)
+
+model_config = {
+        "json_schema_extra":{
+            "example":{
+                "title": "A new book",
+                "author": "Fikre Miko",
+                "description": "The new era of coding",
+                "rating": 5
+                }
+            }
+        }
 
 
 books = [
@@ -35,19 +48,29 @@ books = [
     BOOK(5, "Python and use", "Fikre", "The way to master python", 4.2),
     BOOK(6, "Live death and robote", "Lio", "Learn how to countrol Hardware", 5)
 ]
+@app.get("/")
+def welcome():
+    return "message: Welcome to the book store"
 
 @app.get("/books")
 def list_books():
     return books
 
+@app.post("/book")
+def create_book(book: BookRequest):
+    if len(books) > 0:
+        book.id = books[-1].id + 1
+    else:
+        book.id = 1
+    return book
+
 @app.post("/create_new_book")
 def create_new_book(book: BookRequest):
     new_book = BOOK(**book.model_dump())
-    print(type(new_book))
     books.append(new_book)
-    return new_book
+    return new_booki
 
-@app.post("/create_book")
-def create_book(create_book=Body()):
-    books.append(create_book)
-    return create_book
+
+
+if __name__ == "__main__":
+    uvicorn.run("main2:app", host="127.0.0.1", port=8000, reload=True)
